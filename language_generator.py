dc9b6553e11cacb630772bffb04113fd41375a8a
Federico "EvilScript" Torrielli <evilscript@protonmail.com>
2022-04-11 15:27:22 +0200

    nlg

---
+++ language_generator.py
@@ -0,0 +1,14 @@
+import markovify
+
+
+class NaturalLanguageGenerator:
+    def __init__(self, corpus_path: str):
+        self.text = self.read_text_file(corpus_path)
+        self.text_model = markovify.Text(self.text)
+
+    def read_text_file(self, corpus_path: str):
+        with open(corpus_path, 'r') as f:
+            return f.read()
+
+    def generate_sentence(self) -> str:
+        return self.text_model.make_sentence(tries=100)
